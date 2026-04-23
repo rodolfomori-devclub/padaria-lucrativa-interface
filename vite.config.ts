aea1b919769eb3bc1dc9ce9c406784080639b1d4
Henrique <henriquesouza432@outlook.com>
2026-04-23 10:37:57 -0300

    chore: update Vite configuration to include build sourcemaps and improve path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
-      '~/': '/src/',
-      '@': '/src',
+      "~/": "/src/",
+      "@": "/src",
     },
   },
-})
+  build: {
+    sourcemap: true,
+  },
+});
